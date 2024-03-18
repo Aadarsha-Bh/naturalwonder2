5e9bd622f725ffb55015a27e8ef7d23a6e58d322
Aadarsha-Bh <150773230+Aadarsha-Bh@users.noreply.github.com>
2024-03-18 21:41:12 +0545

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,5 @@
+let supriseMe = document.querySelector('.supriseMe');
+let supriseMeButton = document.querySelector('button');
+let supriseMeFrame = document.querySelector('.supriseImg');
+
+supriseMe.removeChild();
